8e01bdb150ea74b1b35ddfb7101b2c9cfacaa711
Dmitry Boiadji <psih.tm@gmail.com>
2016-10-26 17:40:39 +0300

    ping api method for checking app connectivity

---
+++ Modules/Village/Http/Controllers/Api/V1/PingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Village\Http\Controllers\Api\V1;
+use EllipseSynergie\ApiResponse\Contracts\Response;
+
+class PingController extends ApiController
+{
+    /**
+     * Simple method to check In-app connectivity.
+     * @return Response
+     */
+    public function index()
+    {
+        return;
+    }
+}

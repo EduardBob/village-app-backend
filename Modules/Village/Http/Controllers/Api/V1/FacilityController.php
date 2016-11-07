0bf8a3de8d16e807bf78e9f42a3f90a52dd2b7ff
Dmitry Boiadji <psih.tm@gmail.com>
2016-11-07 11:42:29 +0200

    using db transaction for facility,buildin and user creation steps

--- Modules/Village/Http/Controllers/Api/V1/FacilityController.php
+++ Modules/Village/Http/Controllers/Api/V1/FacilityController.php
@@ -2,18 +2,19 @@
 
 namespace Modules\Village\Http\Controllers\Api\V1;
 
+use DB;
 use Hash;
 use Modules\Village\Entities\AbstractFacility;
 use Modules\Village\Entities\Building;
-use Modules\Village\Entities\Village;
+use Modules\Village\Entities\Sms;
+use Modules\Village\Entities\Token;
 use Modules\Village\Entities\User;
+use Modules\Village\Entities\Village;
+use Modules\Village\Packback\Transformer\TokenTransformer;
 use Modules\Village\Repositories\Eloquent\EloquentUserRoleRepository;
 use Request;
-use Modules\Village\Entities\Sms;
-use Modules\User\Services\UserRegistration;
-use Modules\Village\Entities\Token;
 use Validator;
-use Modules\Village\Packback\Transformer\TokenTransformer;
+
 
 class FacilityController extends VillageController
 {
@@ -41,54 +42,45 @@ class FacilityController extends VillageController
             return $this->response->errorWrongArgs($validator->errors());
         }
         try {
-            // Step 1 create village.
-            $facilityAttributes = ['name' => $data['name'], 'type' => $data['type'], 'active' => 1];
-            $facility           = (new Village)->create($facilityAttributes);
-            // Step 2 create building attached to facility.
-            if ($facility) {
-                $buildingAttributes = ['address' => $facility->name, 'village_id' => $facility->id];
-                $building           = (new Building())->create($buildingAttributes);
-                // Step 3 create user.
-                if ($building) {
-                    $userAttributes   = [
-                      'first_name'  => $firstName,
-                      'last_name'   => $lastName,
-                      'phone'       => $data['phone'],
-                      'email'       => $data['email'],
-                      'building_id' => $building->id,
-                      'village_id'  => $facility->id,
-                      'password'    => Hash::make(str_random())
-                    ];
-                    $userRegistration = app('Modules\User\Services\UserRegistration');
-                    $user             = $userRegistration->register($userAttributes);
-                    $role             = (new EloquentUserRoleRepository())->findBySlug('village-admin');
-                    $user->roles()->sync([$role->id]);
-                    $sms = $this->generateRegistrationTokenAndSendSms($user);
-                    // Step 4 facility admin setup.
-                    $facility->main_admin_id = $user->id;
-                    $facility->save();
+            DB::transaction(function () use ($data, $firstName, $lastName) {
+                // Step 1 create village.
+                $facilityAttributes = ['name' => $data['name'], 'type' => $data['type'], 'active' => 1];
+                $facility           = (new Village)->create($facilityAttributes);
+                // Step 2 create building attached to facility.
+                if ($facility) {
+                    $buildingAttributes = ['address' => $facility->name, 'village_id' => $facility->id];
+                    $building           = (new Building())->create($buildingAttributes);
+                    // Step 3 create user.
+                    if ($building) {
+                        $userAttributes   = [
+                          'first_name'  => $firstName,
+                          'last_name'   => $lastName,
+                          'phone'       => $data['phone'],
+                          'email'       => $data['email'],
+                          'building_id' => $building->id,
+                          'village_id'  => $facility->id,
+                          'password'    => Hash::make(str_random())
+                        ];
+                        $userRegistration = app('Modules\User\Services\UserRegistration');
+                        $user             = $userRegistration->register($userAttributes);
+                        $role             = (new EloquentUserRoleRepository())->findBySlug('village-admin');
+                        $user->roles()->sync([$role->id]);
+                        $this->generateRegistrationTokenAndSendSms($user);
+                        // Step 4 facility admin setup.
+                        $facility->main_admin_id = $user->id;
+                        $facility->save();
+                    }
                 }
-            }
-
+            });
             $this->response->setStatusCode(201);
-            $success = [];
+            $success            = [];
             $success['message'] = trans('village::villages.messages.created');
             return $this->response->withArray($success);
 
         } catch (\Exception $e) {
-            // Rollback created data.
-            if (isset($building)) {
-                $building->forceDelete();
-            }
-            if (isset($user)) {
-                $user->forceDelete();
-            }
-            if (isset($facility)) {
-                $facility->forceDelete();
-            }
-            return $this->response->errorInternalError('facility_registration_error');
+           // return $this->response->errorInternalError('facility_registration_error');
             // More information for debugging.
-            //return $this->response->withError($e->getMessage(), 500);
+            return $this->response->withError($e->getMessage(), 500);
         }
     }
 
